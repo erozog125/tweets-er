2635eb5dc5090475e532d8f9c93f9dac59cc1b30
Edwin Rozo Gómez <erozog125@gmail.com>
2023-02-21 08:03:05 -0500

    Tweets Mockup v.1

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { ContainerTweet } from "./components/ContainerTweet/ContainerTweet"
+
+function App() { 
+
+  return (
+    <div className="App">
+      <h1>Generador de Tweets</h1>
+      <ContainerTweet />      
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/ContainerTweet/ContainerTweet.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const ContainerTweet = () => {
+  return (
+    <div className='container-tweet'>
+
+    </div>
+  )
+}
